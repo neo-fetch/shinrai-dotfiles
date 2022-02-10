98f5750b5a1f6e6062b4b8a9d88f13384d0fdcf0
neo-fetch <mayank18529@mechyd.ac.in>
2022-02-10 22:43:30 +0530

    yet another quality commit

--- dotfiles/qtile/scripts/lock_screen.py
+++ dotfiles/qtile/scripts/lock_screen.py
@@ -7,16 +7,19 @@ def lock_screen():
     A function decorator for Qtile to lock the screen with
     """
     processes = [
-        ['scrot', '/tmp/screen.png'],
+        ["scrot", "/tmp/screen.png"],
         [
-            'convert',
-            '/tmp/screen.png',
-            '-scale', '10%',
-            '-scale', '1000%',
-            '/tmp/screen.png'
+            "convert",
+            "/tmp/screen.png",
+            "-scale",
+            "10%",
+            "-scale",
+            "1000%",
+            "/tmp/screen.png",
         ],
-        ['i3lock', '-i', '/tmp/screen.png'],
-        ['rm', '/tmp/screen.png']
+        ["python", "/home/neo/test.py"],
+        ["i3lock", "-i", "/tmp/screen.png"],
+        ["rm", "/tmp/screen.png"],
     ]
 
     def inner(qtile=None):
@@ -27,6 +30,6 @@ def lock_screen():
     return inner
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     lock = lock_screen()
     lock()
