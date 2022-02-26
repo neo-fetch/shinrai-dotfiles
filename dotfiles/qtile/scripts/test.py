b846ceaafec3309b38827669789e1829206acc41
neo-fetch <mayank18529@mechyd.ac.in>
2022-02-26 14:17:44 +0530

    Generalized joke overlay for any desktop resolution

--- dotfiles/qtile/scripts/test.py
+++ dotfiles/qtile/scripts/test.py
@@ -2,10 +2,10 @@
 import random
 import re
 import sys
+from turtle import width
 import requests
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
-
 def get_joke():
     url = "https://icanhazdadjoke.com/"
     headers = {"Accept": "application/json"}
@@ -16,10 +16,8 @@ def get_joke():
         print("Something went wrong: {}".format(response.status_code))
         sys.exit()
 
-# First script attempts to get the joke from ProgrammerDadJokes subreddit.
-# If that fails, it will get the joke from the next best source: icanhazdadjoke.com
-# I was too lazy to get the joke from the reddit API, so I just grabbed the joke from the website.
-# The downside is that reddit does not always respond with json, so to avoid errors, we have a backup site.
+background = Image.open("/tmp/screen.png")
+width, height = background.size
 
 try:
     jsondata = requests.get('https://www.reddit.com/r/ProgrammerDadJokes.json').json()
@@ -28,7 +26,7 @@ try:
     i = random.choice(df['data']['children'])
     # print(f"{i['data']['title']}\n\t{re.sub('&amp;#x200B;', '', i['data']['selftext'])}\n")
     
-    img = Image.new('RGB', (2560, 1080), color = (69, 39, 44))
+    img = Image.new('RGB', (width, height), color = (69, 39, 44))
     
     fnt = ImageFont.truetype('/usr/share/fonts/noto/NotoSansMono-Bold.ttf', 15)
     d = ImageDraw.Draw(img)
@@ -36,21 +34,19 @@ try:
 
     img.save('/tmp/joke.png')
 except:
-    img = Image.new('RGB', (2560, 1080), color = (69, 39, 44))
+    img = Image.new('RGB', (width, height), color = (69, 39, 44))
     
     fnt = ImageFont.truetype('/usr/share/fonts/noto/NotoSansMono-Bold.ttf', 15)
     d = ImageDraw.Draw(img)
-    d.text((960,405), get_joke()["joke"], font=fnt, fill=(248, 228, 215))
+    d.text(((width)/2,height/2), get_joke()["joke"], font=fnt, fill=(248, 228, 215))
 
     img.save('/tmp/joke.png')
 
 
-background = Image.open("/tmp/screen.png")
 overlay = Image.open("/tmp/joke.png")
 
 background = background.convert("RGBA")
 overlay = overlay.convert("RGBA")
 
-# 0.2 is the degree of merging of overlay with background. 0.0 is completely transparent, 1.0 is completely opaque.
-new_img = Image.blend(background, overlay, 0.2) 
+new_img = Image.blend(background, overlay, 0.5)
 new_img.save("/tmp/screen.png","PNG")
